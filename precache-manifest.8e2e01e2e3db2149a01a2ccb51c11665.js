f3721ba20a7a6e006e16dbc8bd9eacbcdd42cf24
Estefania Osses <estefania.osses.v@gmail.com>
2019-07-16 22:28:44 -0400

    Updates

---
+++ precache-manifest.8e2e01e2e3db2149a01a2ccb51c11665.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1934c43709a94c8d5c47",
+    "url": "/finciero/static/css/main.e08e3650.chunk.css"
+  },
+  {
+    "revision": "1934c43709a94c8d5c47",
+    "url": "/finciero/static/js/main.9d0c19a7.chunk.js"
+  },
+  {
+    "revision": "f49a0bb2b620906dd84f",
+    "url": "/finciero/static/js/runtime~main.f1ee5ef3.js"
+  },
+  {
+    "revision": "fb400f1eac9b21789fe7",
+    "url": "/finciero/static/js/2.bb7eff4e.chunk.js"
+  },
+  {
+    "revision": "9d830b5ba6da2f64511b95d134dfb01a",
+    "url": "/finciero/static/media/logo3.9d830b5b.svg"
+  },
+  {
+    "revision": "abddd0cfb5b514bc2a5597343823e742",
+    "url": "/finciero/static/media/amazon.abddd0cf.png"
+  },
+  {
+    "revision": "379c774757fb00d88d51cfb5d318dd10",
+    "url": "/finciero/static/media/ebay.379c7747.png"
+  },
+  {
+    "revision": "c76f48827261138f0f81229dbc3c824c",
+    "url": "/finciero/static/media/steam.c76f4882.png"
+  },
+  {
+    "revision": "008bdfff16e4148f6de9e19437ecfeb9",
+    "url": "/finciero/static/media/app_store.008bdfff.png"
+  },
+  {
+    "revision": "4ec6b576619906b9a36522db23aa5ffe",
+    "url": "/finciero/static/media/aliexpress.4ec6b576.png"
+  },
+  {
+    "revision": "5053609f207d9865d5f3129d9af0ac87",
+    "url": "/finciero/static/media/despegar.5053609f.png"
+  },
+  {
+    "revision": "77ccfcbc6251d48b03a685fcbb5c5d24",
+    "url": "/finciero/static/media/itunes.77ccfcbc.png"
+  },
+  {
+    "revision": "5651147964cb73f605089b47df4fe70f",
+    "url": "/finciero/static/media/netflix.56511479.png"
+  },
+  {
+    "revision": "670b15e6a4aa68089c2d2339ca07f560",
+    "url": "/finciero/static/media/paypal.670b15e6.png"
+  },
+  {
+    "revision": "a955cffbd529f6622fb8230ce48de8ed",
+    "url": "/finciero/static/media/max_bg.a955cffb.png"
+  },
+  {
+    "revision": "a267d38453f2b5e51b24023e7b77f376",
+    "url": "/finciero/index.html"
+  }
+];
